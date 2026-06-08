0356aeb1ea9c5dab391d740dfe795512819e65aa
Q-azwsx <3510959274@qq.com>
2026-06-08 10:08:11 +0800

    feat(ble): add hardware reset

--- Drivers/AT_TB05/tb05.h
+++ Drivers/AT_TB05/tb05.h
@@ -4,7 +4,7 @@
 #include "at.h"
 #include "BLE/at_ble.h"
 
-#define TB05_HW_RST()
+#define TB05_HW_RST() do { HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET); HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);} while(0)
 #define TB05_BLINK(L)
 
 // Common fucntions
