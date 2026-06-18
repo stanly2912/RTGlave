13f0486928949d70747025ce0808dd2f66bbacd6
Q-azwsx <3510959274@qq.com>
2026-06-18 19:48:21 +0800

    feat(app): add func1 select action

--- Core/Src/glave.c
+++ Core/Src/glave.c
@@ -1,13 +1,17 @@
 #include "glave.h"
 #include "BLE/at_ble.h"
+#include "at.h"
 #include "protocol.h"
 
 
 #include "rthw.h"
 #include "main.h"
 #include "rtthread.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_def.h"
 #include "tb05.h"
 #include <stdint.h>
+#include <string.h>
 
 extern UART_HandleTypeDef huart2;
 
@@ -36,7 +40,8 @@ void input_monitor(void *keycode) {
     }
 }
 
-static uint8_t packetbuf[1024];
+static uint8_t packbuf[1024];
+const uint8_t resp[4] = {SQB_HEAD, SQB_TYPE_RESP, 0, 0};
 
 void glave_main(void *keycode) {
     // Declarations
@@ -50,6 +55,13 @@ void glave_main(void *keycode) {
     
     while (at_wait_ok() != AT_TIMEOUT) {}
     tb05_force_scan(0, "RTHelmet", remote_mac[0]);
+    tb05_force_scan(0, "RTTap", remote_mac[1]);
+
+    while (tb05_connect(0, remote_mac[0]) != AT_OK) {
+        tb05_disconnect(0);
+        rt_thread_delay(1000);
+    }
+
     rt_exit_critical();
     
     rt_thread_startup(th_input);
@@ -57,6 +69,7 @@ void glave_main(void *keycode) {
     while (1) {
         rt_enter_critical();
         keycopy = *(uint32_t *) keycode;
+        
 
         switch (keycopy) {
             case key_none: break;
@@ -65,18 +78,93 @@ void glave_main(void *keycode) {
                     if (cur_func > 2) {
                         cur_func = 0;
                     }
-                    int size = sqb_pack(packetbuf, SQB_TYPE_SWITCH, sizeof(cur_func), &cur_func);
-                    tb05_force_connect(0, remote_mac[0]);
-                    tb05_disconnect(0);
+
+                    int size = sqb_pack(packbuf, SQB_TYPE_SWITCH, sizeof(cur_func), &cur_func);
+                    int ret, blestate;
+                    at_exit_tranfer(0);
+                    ret = at_blestate(0, &blestate);
+                    if (ret != AT_OK) break;
+                    if (blestate == 0) {
+                        at_bleconnect(0, remote_mac[0]);
+                    }
+                    else {
+                        at_enter_tranfer(0);
+                    }
+                    at_send(0, packbuf, size);
                 }
                 break;
             case key_select:
                 switch (cur_func) {
                     case 0: // 疲劳检测
+                    {
+                        int ret, blestate, size;
+                        at_exit_tranfer(0);
+                        ret = at_blestate(0, &blestate);
+                        if (ret != AT_OK) break;
+                        if (blestate != 0) {
+                            tb05_disconnect(0);
+                        }
+                        ret = at_bleconnect(0, remote_mac[1]);//改为 remote_mac[1]
+                        if (ret == AT_OK) {
+                            size = sqb_pack(packbuf, SQB_TYPE_DATA, 0, NULL);
+                            at_send(0, packbuf, size);
+                            const uint8_t header = SQB_HEAD;
+                            uint8_t *rcv = at_wait_for(&header, 1, AT_TIMEOUT_TIME);
+                            if (rcv != NULL) {
+                                uint32_t begin = HAL_GetTick();
+                                while (at_readable_len() < 4) {
+                                    if (HAL_GetTick() - begin > AT_TIMEOUT_TIME) {
+                                        at_pop(at_readable_len());
+                                        break;
+                                    }
+                                }
+                                if (at_readable_len() >= 4) {
+                                    memcpy(packbuf, rcv, 4);
+                                    at_pop(4);
+                                    if (sqb_type(packbuf) == SQB_TYPE_RESP) {
+                                        while (at_readable_len() < sqb_bodylen(packbuf)) {
+                                            if (HAL_GetTick() - begin > AT_TIMEOUT_TIME) {
+                                                at_pop(at_readable_len());
+                                                break;
+                                            }
+                                        }
+                                        if (at_readable_len() >= sqb_bodylen(packbuf)) {
+                                            memcpy(sqb_body(packbuf), at_readptr(), sqb_bodylen(packbuf));
+                                            at_pop(sqb_bodylen(packbuf));
+
+                                            /* 开始语音播报 */
+                                            uint8_t body[2] = {0, sqb_body(packbuf)[0]};
+                                            size = sqb_pack(packbuf, SQB_TYPE_SELECT, sizeof body, body);
+                                            tb05_disconnect(0);
+ 
+                                            at_bleconnect(0, remote_mac[0]);
+                                        }
+                                        else {
+                                            at_pop(at_readable_len());
+                                        }
+                                    }
+                                }
+                            }
+                            tb05_disconnect(0);
+                        }
+                    }
                     break;
                     case 1: // 心率
                     case 2: // 血氧
-
+                    {
+                        int ret, blestate, size;
+                        size = sqb_pack(packbuf, SQB_TYPE_SELECT, sizeof(cur_func), &cur_func);
+                        at_exit_tranfer(0);
+                        ret = at_blestate(0, &blestate);
+                        if (ret != AT_OK) break;
+                        if (blestate == 0) {
+                            at_bleconnect(0, remote_mac[0]);
+                        }
+                        else {
+                            at_enter_tranfer(0);
+                        }
+                        at_send(0, packbuf, size);
+                    }
                     default: break;
                 }
                 break;
